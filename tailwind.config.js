f8ede4b80792e2ef91e1aa2bc18633a63ca34f48
rajeevranjan413 <rajuranjan413@gmail.com>
2025-04-20 22:22:46 +0530

    revision

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1e2641',
+        secondary: '#1e517e',
+        accent: '#ffc107',
+        background: '#f1f2fc',
+        text: '#333333',
+        border: '#3a456a',
+      },
+      fontFamily: {
+        sans: ['IBM Plex Sans', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
+    },
   },
   plugins: [],
 }
